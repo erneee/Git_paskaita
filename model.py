3a8bdbca8e22c33a9826f247681c85aad04fe2b8
Ernestas <ernestasvvv@gmail.com>
2023-08-18 10:12:23 +0300

    Ideta class Produktai

--- model.py
+++ model.py
@@ -12,9 +12,17 @@ class Klientas(Base):
     f_name = Column("vardas", String)
     email = Column("el_pastas", String, unique=True)
 
-
+    class Produktai(Base):
+        __tablename__ = 'produktai'
+        id = Column(Integer, primary_key=True)
+        name = Column("pavadinimas", String)
+        price = Column("kaina", Float)
+        order_products = relationship('OrderProduct', back_populates='product')
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# git commit -m "" parasytu ka padareme
+# git config --global user.email "ernestasvvv@gmail.com"
+# git config --global user.name "Ernestas"
